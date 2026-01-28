d2e73cd520cc169b5658dcb29137aa515c193ac9
Sivaprava01 <sivaprava2006@gmail.com>
2026-01-28 18:26:34 +0530

    First commit regarding social authentication using clerk

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { userRouter } from './APIs/UserAPI.js';
+import cors from 'cors';
+
+
+// create express app FIRST
+const app = express();
+
+// port number
+const PORT = 4000;
+
+// middleware
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.json());
+app.use('/user-api', userRouter);
+
+// test route
+app.get("/test", (req, res) => {
+  res.json({ message: "Server is working fine!" });
+});
+
+// db connection
+async function connectDB() {
+  await mongoose.connect("mongodb://localhost:27017/vnrblog2026");
+  console.log("DB connection success");
+
+  app.listen(PORT, () =>
+    console.log(`Server is running on port ${PORT}`)
+  );
+}
+
+connectDB();
